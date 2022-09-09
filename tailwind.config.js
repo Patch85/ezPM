d4a2721111dcbb56dfaa3f082450be21fe2d9386
Dillon A. John <dillon.a.john@gmail.com>
2022-09-09 17:48:02 -0400

    0.4.0 - Laravel Breeze
    - Installed Breeze via composer
    - Ran migrations
    - removed Alpine.js lines from bootstrap

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,23 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        './resources/views/**/*.blade.php',
+        './storage/framework/views/*.php',
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [require('@tailwindcss/forms')],
+};
