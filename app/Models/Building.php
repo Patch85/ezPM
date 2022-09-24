b7dd314f1f8a73ac4a34a0402efb51249be28c5e
Dillon A. John <dillon.a.john@gmail.com>
2022-09-24 13:28:03 -0400

    0.7.10 - Buildings: IDE Helper
    - Generated a docblock for the Building Model

--- app/Models/Building.php
+++ app/Models/Building.php
@@ -5,6 +5,31 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Building
+ *
+ * @property int $id
+ * @property string $building_number
+ * @property string|null $description
+ * @property string|null $address
+ * @property string $status
+ * @property int $floors
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Database\Factories\BuildingFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Building newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Building newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Building query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Building whereAddress($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Building whereBuildingNumber($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Building whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Building whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Building whereFloors($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Building whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Building whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Building whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Building extends Model
 {
     use HasFactory;
