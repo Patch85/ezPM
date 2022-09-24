2f78c76279e99ba96bbb77b5288ad3a39765cccf
Dillon A. John <dillon.a.john@gmail.com>
2022-09-24 14:47:01 -0400

    0.7.11 - Buildings: Index view updates
    - Updated styles for buildings.index to increase readability

--- resources/views/buildings/index.blade.php
+++ resources/views/buildings/index.blade.php
@@ -7,9 +7,9 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-
-            <table class="w-full table-auto">
+        <div
+            class="mx-auto flex w-full max-w-7xl justify-center border-b border-gray-200 bg-white p-6 shadow-sm hover:shadow-indigo-400 sm:rounded-lg lg:px-8">
+            <table class="m-4 table-auto border-b border-gray-200 bg-gray-50 p-4 shadow-sm sm:rounded-lg md:w-4/5">
                 <thead>
                     <tr>
                         <th class="text-md font-semibold">
@@ -30,8 +30,9 @@
                     @foreach ($buildings as $building)
                         <tr class="text-center text-slate-800 odd:bg-slate-100 even:bg-white hover:bg-indigo-300">
                             <td class="hover:text-indigo-700">
-                                <a
-                                    href="{{ route('buildings.show', $building) }}">{{ __($building->building_number) }}</a>
+                                <a href="{{ route('buildings.show', $building) }}">
+                                    {{ __($building->building_number) }}
+                                </a>
                             </td>
                             <td>{{ __($building->description) }}</td>
                             <td>{{ __($building->status) }}</td>
