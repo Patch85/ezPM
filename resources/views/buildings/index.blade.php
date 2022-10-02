78dfb2c35e55c7b452fadd39e80020f2e0add58d
Dillon A. John <dillon.a.john@gmail.com>
2022-10-01 20:14:21 -0400

    0.8.10 - Buildings: Index View Cleanup
    - Refactored the buildings.index blade view to use the new
      blade components for tables and content-panel

--- resources/views/buildings/index.blade.php
+++ resources/views/buildings/index.blade.php
@@ -6,48 +6,41 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div
-            class="mx-auto flex w-full max-w-7xl justify-center border-b border-gray-200 bg-white p-6 shadow-sm hover:shadow-indigo-400 sm:rounded-lg lg:px-8">
-            <table class="m-4 table-auto border-b border-gray-200 bg-gray-50 p-4 shadow-sm sm:rounded-lg md:w-4/5">
-                <thead>
-                    <tr>
-                        <th class="text-md font-semibold">
-                            {{ __('Building Number') }}
-                        </th>
-
-                        <th class="text-md font-semibold">
-                            {{ __('Description') }}
-                        </th>
-
-                        <th class="text-md font-semibold">
-                            {{ __('Status') }}
-                        </th>
-                    </tr>
-                </thead>
-
-                <tbody>
-                    @foreach ($buildings as $building)
-                        <tr class="text-center text-slate-800 odd:bg-slate-100 even:bg-white hover:bg-indigo-300">
-                            <td class="text-indigo-500 hover:text-indigo-700">
-                                <a href="{{ route('buildings.show', $building) }}">
-                                    {{ __($building->building_number) }}
-                                </a>
-                            </td>
-                            <td>{{ __($building->description) }}</td>
-                            <td>{{ __($building->status) }}</td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-
-        <div class="mx-auto mt-8 max-w-7xl sm:px-6 lg:px-8">
-            <a href="{{ route('buildings.create') }}" class="">
-                <x-primary-button>
-                    Add a Building
-                </x-primary-button>
-            </a>
-        </div>
+    <div class="ml-12 mt-4">
+        <a href="{{ route('buildings.create') }}">
+            <x-primary-button>Add a Building</x-primary-button>
+        </a>
     </div>
+
+    <x-content-panel class="m-4 py-4">
+        <x-table.table>
+            <x-slot name="thead">
+                <x-table.tr>
+                    <x-table.th>{{ __('Building') }}</x-table.th>
+
+                    <x-table.th>{{ __('Description') }}</x-table.th>
+
+                    <x-table.th>{{ __('Status') }}</x-table.th>
+                </x-table.tr>
+            </x-slot>
+
+            <x-slot name="tbody">
+                @foreach ($buildings as $building)
+                    <x-table.tr class="odd:bg-slate-100 even:bg-white hover:bg-indigo-300">
+
+                        <x-table.td class="text-right text-indigo-500 hover:text-indigo-700">
+                            <a href="{{ route('buildings.show', $building) }}">
+                                {{ __($building->building_number) }}
+                            </a>
+                        </x-table.td>
+
+                        <x-table.td class="text-left">{{ __($building->description) }}</x-table.td>
+
+                        <x-table.td class="text-left">{{ __($building->status) }}</x-table.td>
+
+                    </x-table.tr>
+                @endforeach
+            </x-slot>
+        </x-table.table>
+    </x-content-panel>
 </x-app-layout>
