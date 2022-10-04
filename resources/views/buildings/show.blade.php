b1bb897ac0e602a993263aae286ea67c98e54a11
Dillon A. John <dillon.a.john@gmail.com>
2022-10-04 11:08:20 -0400

    0.8.11 - Buildings - Show View
    - Refactored the buildings.show blade view to utilize the content-panel
      blade component0.8.11 - Buildings - Show View
    
    - Refactored the buildings.show blade view to utilize the content-panel
      blade component0.8.11 - Buildings - Show View
    
    - Refactored the buildings.show blade view to utilize the content-panel
      blade component0.8.11 - Buildings - Show View
    
    - Refactored the buildings.show blade view to utilize the content-panel
      blade component0.8.11 - Buildings - Show View
    
    - Refactored the buildings.show blade view to utilize the content-panel
      blade component0.8.11 - Buildings - Show View
    
    - Refactored the buildings.show blade view to utilize the content-panel
      blade component0.8.11 - Buildings - Show View
    
    - Refactored the buildings.show blade view to utilize the content-panel
      blade component0.8.11 - Buildings - Show View
    
    - Refactored the buildings.show blade view to utilize the content-panel
      blade component0.8.11 - Buildings - Show View
    
    - Refactored the buildings.show blade view to utilize the content-panel
      blade component0.8.11 - Buildings - Show View
    
    - Refactored the buildings.show blade view to utilize the content-panel
      blade component0.8.11 - Buildings - Show View
    
    - Refactored the buildings.show blade view to utilize the content-panel
      blade component

--- resources/views/buildings/show.blade.php
+++ resources/views/buildings/show.blade.php
@@ -6,9 +6,8 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-
+    <x-content-panel class="m-4 py-4">
+        <div>
             <h3 class="text-lg font-semibold text-slate-600">Building {{ $building->building_number }} -
                 <em>{{ $building->status }}</em>
             </h3>
@@ -18,69 +17,64 @@
             <h4 class="text-md mt-2 font-medium text-slate-500">{{ $building->description }}</h4>
 
             <p class="text-md mt-2 font-medium">Floors: {{ $building->floors }}</p>
-
-            <div class="mt-4">
-                <a href="{{ route('buildings.edit', $building) }}">
-                    <x-primary-button>
-                        Edit
-                    </x-primary-button>
-                </a>
-            </div>
         </div>
 
-        <div class="mx-auto my-4 max-w-7xl sm:px-6 lg:px-8">
-
-            <div
-                class="mx-auto flex w-full max-w-7xl justify-center border-b border-gray-200 bg-white p-6 shadow-sm hover:shadow-indigo-400 sm:rounded-lg lg:px-8">
-
-                <table class="m-4 table-auto border-b border-gray-200 bg-gray-50 p-4 shadow-sm sm:rounded-lg md:w-4/5">
-                    <thead>
-                        <tr>
-                            <th class="text-md font-semibold">
-                                {{ __('Floor') }}
-                            </th>
-
-                            <th class="text-md font-semibold">
-                                {{ __('Room Number') }}
-                            </th>
-
-                            <th class="text-md font-semibold">
-                                {{ __('Name') }}
-                            </th>
-
-                            <th class="text-md font-semibold">
-                                {{ __('Type') }}
-                            </th>
-
-                            <th class="text-md font-semibold">
-                                {{ __('PM Status') }}
-                            </th>
-                        </tr>
-                    </thead>
-
-                    <tbody>
-                        @foreach ($building->equipment as $equipment)
-                            <tr class="text-center text-slate-800 odd:bg-slate-100 even:bg-white hover:bg-indigo-300">
-
-                                <td>{{ __($equipment->floor) }}</td>
-
-                                <td>{{ __($equipment->room_number) }}</td>
-
-                                <td class="text-indigo-500 hover:text-indigo-700">
-                                    <a href="{{ route('equipment.show', $equipment) }}">
-                                        {{ __($equipment->name) }}
-                                    </a>
-                                </td>
-
-                                <td>{{ __($equipment->type) }}</td>
-
-                                <td>{{ __($equipment->pm_status) }}</td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
+        <div class="mt-4">
+            <a href="{{ route('buildings.edit', $building) }}">
+                <x-primary-button>
+                    Edit
+                </x-primary-button>
+            </a>
         </div>
+    </x-content-panel>
 
-    </div>
+    <x-content-panel>
+
+        <table class="m-4 table-auto border-b border-gray-200 bg-gray-50 p-4 shadow-sm sm:rounded-lg md:w-4/5">
+            <thead>
+                <tr>
+                    <th class="text-md font-semibold">
+                        {{ __('Floor') }}
+                    </th>
+
+                    <th class="text-md font-semibold">
+                        {{ __('Room Number') }}
+                    </th>
+
+                    <th class="text-md font-semibold">
+                        {{ __('Name') }}
+                    </th>
+
+                    <th class="text-md font-semibold">
+                        {{ __('Type') }}
+                    </th>
+
+                    <th class="text-md font-semibold">
+                        {{ __('PM Status') }}
+                    </th>
+                </tr>
+            </thead>
+
+            <tbody>
+                @foreach ($building->equipment as $equipment)
+                    <tr class="text-center text-slate-800 odd:bg-slate-100 even:bg-white hover:bg-indigo-300">
+
+                        <td>{{ __($equipment->floor) }}</td>
+
+                        <td>{{ __($equipment->room_number) }}</td>
+
+                        <td class="text-indigo-500 hover:text-indigo-700">
+                            <a href="{{ route('equipment.show', $equipment) }}">
+                                {{ __($equipment->name) }}
+                            </a>
+                        </td>
+
+                        <td>{{ __($equipment->type) }}</td>
+
+                        <td>{{ __($equipment->pm_status) }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </x-content-panel>
 </x-app-layout>
