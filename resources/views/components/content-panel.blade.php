7a252beb0fd3a83382c3e6ca5dd62ca6f4df402a
Dillon A. John <dillon.a.john@gmail.com>
2022-10-04 11:16:33 -0400

    0.8.12 - Blade Components: Content Panel (layout)
    - removed default layout classes from the content-panel blade component.
      - was defaulted to flex, justify-center
    - This allows for more flexible use of the component

--- resources/views/components/content-panel.blade.php
+++ resources/views/components/content-panel.blade.php
@@ -1,7 +1,7 @@
 <div
     {{ $attributes->merge([
         'class' =>
-            'mx-auto flex w-full max-w-7xl justify-center border-b border-gray-200 bg-white p-6 shadow-sm hover:shadow-indigo-400 sm:rounded-lg lg:px-8',
+            'mx-auto w-full max-w-7xl border-b border-gray-200 bg-white p-6 shadow-sm hover:shadow-indigo-400 sm:rounded-lg lg:px-8',
     ]) }}>
 
     {{ $slot }}
