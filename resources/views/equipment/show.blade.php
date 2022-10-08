06ba003f642769219562dd005eba5164f04d21f9
Dillon A. John <dillon.a.john@gmail.com>
2022-10-08 09:59:39 -0400

    0.8.19 - Blade Views: equipment.show Update
    - Added a edit button/link
    - Refactored the layout to provide more information in a format
      that should be easier to scan quickly

--- resources/views/equipment/show.blade.php
+++ resources/views/equipment/show.blade.php
@@ -4,20 +4,53 @@
         <h2 class="text-xl font-semibold leading-tight text-slate-600">
             {{ __('Equipment Details') }}
         </h2>
-    </x-slot>
 
-    <x-content-panel class="m-4 py-4">
-        <h3 class="text-lg font-semibold text-slate-600">
-            {{ $equipment->name }} - <em>{{ $equipment->pm_status }}</em> - <strong>{{ $equipment->type }}</strong>
-        </h3>
+        @if ($buildingNumber = $equipment->building?->building_number)
+            <h3 class="text-lg font-semibold text-slate-600">
+                Building {{ $buildingNumber }}
+            </h3>
+        @endif
 
         <h4 class="text-md font-semibold text-slate-600">
             Floor {{ $equipment->floor }} :
             Room {{ $equipment->room_number }}
         </h4>
+    </x-slot>
 
-        <p class="text-bold text-sm uppercase">{{ $equipment->functional_status }}</p>
+    <x-content-panel class="m-4 py-4">
+        <div class="flex justify-evenly">
+            <p class="text-lg font-semibold text-slate-600">
+                <strong>Name</strong> <br>
+                {{ $equipment->name }}
+            </p>
 
-        <p class="mt-2 text-sm font-normal">{{ $equipment->description }}</p>
+            <p class="text-lg font-semibold text-slate-600">
+                <strong>Type</strong> <br>
+                {{ $equipment->type }}
+            </p>
+
+            <p class="text-lg font-semibold text-slate-600">
+                <strong>PM</strong> <br>
+                {{ $equipment->pm_status }}
+            </p>
+
+            <p class="text-lg font-semibold text-slate-600">
+                <strong>Condition</strong> <br>
+                {{ $equipment->functional_status }}
+            </p>
+        </div>
+
+        <p class="mt-2 text-sm font-normal">
+            <strong class="underline">Description</strong> <br>
+            {{ $equipment->description }}
+        </p>
+
+        <div class="mt-4 mr-4 flex justify-end">
+            <a href="{{ route('equipment.edit', $equipment) }}">
+                <x-primary-button>
+                    Edit
+                </x-primary-button>
+            </a>
+        </div>
     </x-content-panel>
 </x-app-layout>
