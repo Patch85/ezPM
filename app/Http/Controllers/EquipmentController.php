b441b262c3aa877c414f0df3759dcaa466b10773
Dillon A. John <dillon.a.john@gmail.com>
2022-10-09 13:49:39 -0400

    0.8.25 - Equipment: Update
    - Replaced EquipmentController's update() stub with logic for updating
      an existing resource with validated data as receieved from the edit
      form
    - Added a validtion method, validateEquipmentData(), to
      EquipmentController

--- app/Http/Controllers/EquipmentController.php
+++ app/Http/Controllers/EquipmentController.php
@@ -7,8 +7,14 @@ use App\Models\Equipment;
 use Illuminate\Contracts\View\View;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\Container\BindingResolutionException;
+use Illuminate\Database\Eloquent\MassAssignmentException;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Validation\Rule;
+use Symfony\Component\Routing\Exception\RouteNotFoundException;
 
+/** @package App\Http\Controllers */
 class EquipmentController extends Controller
 {
 
@@ -84,7 +90,23 @@ class EquipmentController extends Controller
     }
 
     /**
-     * Update the specified resource in storage.
+     * Update the equipment in storage
+     *
+     * @param Request $request
+     * @param Equipment $equipment
+     * @return RedirectResponse
+     * @throws MassAssignmentException
+     * @throws BindingResolutionException
+     * @throws RouteNotFoundException
+     */
+    public function update(Request $request, Equipment $equipment): RedirectResponse
+    {
+        $attributes = $this->validateEquipmentData($request, $equipment);
+
+        $equipment->update($attributes);
+
+        return redirect()->route('equipment.show', ['equipment' => $equipment])->with('success', "Equipment $equipment->name updated");
+    }
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \App\Models\Equipment  $equipment
@@ -96,13 +118,44 @@ class EquipmentController extends Controller
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Custom validation rules for basic equipment data
      *
-     * @param  \App\Models\Equipment  $equipment
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param null|Equipment $equipment
+     * @return array
      */
-    public function destroy(Equipment $equipment)
+    protected function validateEquipmentData(Request $request, ?Equipment $equipment = null): array
     {
-        //
+        $equipment ??= new Equipment();
+
+        $validated = $request->validate([
+            'name' => [
+                'required',
+                'max:255',
+                Rule::unique('equipment', 'name')
+                    ->ignore($equipment?->id)
+                    ->where('building_id', $equipment?->building?->id)
+            ],
+            'type' => ['required', Rule::in(Equipment::$equipmentTypes)],
+            'description' => ['required', 'max:500'],
+            'room_number' => ['sometimes', 'max:10'],
+            'floor' => ['sometimes', 'max:3'],
+            'functional_status' => ['required', Rule::in(Equipment::$functionalStatuses)],
+            'pm_status' => ['required', Rule::in(Equipment::$pmStatuses)],
+            'building_id' => ['required', Rule::exists('buildings', 'id')]
+        ], $request->only([
+            'name',
+            'type',
+            'description',
+            'room_number',
+            'floor',
+            'functional_status',
+            'pm_status',
+            'building_id',
+        ]));
+
+        $validated['slug'] = Str::slug($validated['name']);
+
+        return $validated;
     }
 }
